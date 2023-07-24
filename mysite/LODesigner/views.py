21d36aef66fcde8afeb452ec4813d8cb15b79e73
StudioShader <studioshader2018@gmail.com>
2023-07-24 16:56:31 -0300

    stable side bar

--- mysite/LODesigner/views.py
+++ mysite/LODesigner/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.decorators.clickjacking import xframe_options_sameorigin
 
 def index(request):
     context = {"lo": [1, 2, 3]}
-    return render(request, "index.html", context)
+    return render(request, "projects_list.html", context)
+
+@xframe_options_sameorigin
+def lodesigner(request):
+    return render(request, "lo_designer.html")
